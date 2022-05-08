79101f58754c745db00624c7e487b000294ee8c1
Narala Govardhan Reddy <reddygovardhan6826@gmail.com>
2022-05-08 18:10:16 +0530

    Added Building Blocks for messaging Broker

---
+++ BuildingBlocks/MessagingBrokerDefaults/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagingBrokerDefaults.Models
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+        }
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
